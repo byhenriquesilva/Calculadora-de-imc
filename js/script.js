9df292acc3c7af878152986282798337ecc8122d
Henrique Silva <92489702+byhenriquesilva@users.noreply.github.com>
2022-10-23 23:28:24 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function start() {
+
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  inputWeight.addEventListener('input', handleCalculateImc);
+  inputHeight.addEventListener('input', handleCalculateImc);
+
+  handleCalculateImc();
+}
+
+function handleCalculateImc() {
+  var inputWeight = Number(document.querySelector('#input-weight').value);
+  var inputHeight = Number(document.querySelector('#input-height').value);
+
+  var imc = calculateImc(inputWeight, inputHeight);
+  
+  var resultImc = document.querySelector('#imc-result');
+  resultImc.textContent = imc.toFixed(2).replace('.', ',');
+}
+
+function calculateImc(weight, height) {
+  return weight / (height * height);
+}
+
+start();
